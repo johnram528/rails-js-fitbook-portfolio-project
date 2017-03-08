5e909401f3c008d2c65ca17f32fc91e25963fa03
johnram528 <johnram528@gmail.com>
2017-03-08 12:44:33 -0500

    added application_helper to routine serializer and added user relationship

--- app/serializers/routine_serializer.rb
+++ app/serializers/routine_serializer.rb
@@ -1,4 +1,12 @@
 class RoutineSerializer < ActiveModel::Serializer
-  attributes :id, :name, :estimated_time, :user_id
+  include ApplicationHelper
+
+  attributes :id, :name, :estimated_time, :created_by
   has_many :exercises
+  belongs_to :user
+
+  def estimated_time
+    show_estimated_time(object)
+  end
+
 end
