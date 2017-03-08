3c153327de168ada4e8355a302bce0a7aa363106
johnram528 <johnram528@gmail.com>
2017-03-08 12:09:56 -0500

    added reps attribute to exercise serializer

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,3 +1,7 @@
 class ExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :instructions, :rep_time, :muscles, :routine_exercises
+  attributes :id, :name, :instructions, :rep_time, :muscles, :reps
+
+  def reps
+    object.routine_exercises[0].reps
+  end
 end
