2b0b6524b2239f24f0ddf10bd470b7e0eacc492d
johnram528 <johnram528@gmail.com>
2017-03-08 12:32:15 -0500

    added application_helper to exercise serialize and use show_estimated_time heler to set new attribute

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,7 +1,12 @@
 class ExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :instructions, :rep_time, :muscles, :reps
+  include ApplicationHelper
+  attributes :id, :name, :instructions, :rep_time, :muscles, :reps, :estimated_time
 
   def reps
     object.routine_exercises[0].reps
+  end
+
+  def estimated_time 
+    show_estimated_time(object)
   end
 end
